38cc16b82c4aba438530b5d78733f77cd5c3b33f
InsprowareGit <kumar.alok.16a@gmail.com>
2026-07-14 17:09:35 +0530

    Third update.

--- react_sample_two/src/App.jsx
+++ react_sample_two/src/App.jsx
@@ -1,23 +1,61 @@
 import React from 'react'
 import './App.css'
 import Card from './components/Card/Card'
-import './components/Card/Card.css'
 
 const App = () => {
+  const cards = [
+    {
+      title: 'One',
+      description: 'This is the description for Card One.'
+    },
+    {
+      title: 'Two',
+      description: 'This is the description for Card Two.'
+    },
+    {
+      title: 'Three',
+      description: 'This is the description for Card Three.'
+    },
+    {
+      title: 'Four',
+      description: 'This is the description for Card Four.'
+    },
+    {
+      title: 'Five',
+      description: 'This is the description for Card Five.'
+    },
+    {
+      title: 'Six',
+      description: 'This is the description for Card Six.'
+    },
+    {
+      title: 'Seven',
+      description: 'This is the description for Card Seven.'
+    },
+    {
+      title: 'Eight',
+      description: 'This is the description for Card Eight.'
+    }
+  ]
+
   return (
     <div className="app">
       <h3 className="title">Welcome Alok</h3>
       <h1 className="subtitle">BICYCLE CHALLENGE</h1>
 
       <div className="card-container">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+
+      {cards.map(
+          (elem, index) => {
+            return (
+              <Card key={index}
+              title={elem.title}
+              description={elem.description} />
+            )
+          }
+        )
+
+      }
       </div>
     </div>
   )
--- react_sample_two/src/components/Card/Card.jsx
+++ react_sample_two/src/components/Card/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import styles from './Card.module.css'
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div className="card">
-        <img className="card-image" src="https://th.bing.com/th/id/OIP.5Eddf-iZqa_MN20nbwtGiwHaNK?w=187&h=333&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="Card Image" />
-        <h5 className="card-title">Card Title</h5>
-        <p className="card-description">lore ipsum dolor sit amet</p>
+    <div className={styles.card}
+      key={props.key}>
+      <img className={styles['card-image']} src="https://th.bing.com/th/id/OIP.5Eddf-iZqa_MN20nbwtGiwHaNK?w=187&h=333&c=7&r=0&o=7&dpr=1.3&pid=1.7&rm=3" alt="Card Image" />
+      <h5 className={styles['card-title']}>{props.title}</h5>
+      <p className={styles['card-description']}>{props.description}</p>
     </div>
   )
 }
